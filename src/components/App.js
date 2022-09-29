e4ec5ad30527b9dfa77559f634e175d4ed986264
higorpr <higorpr@gmail.com>
2022-09-28 23:22:09 -0300

    Initial Commit - Pushing Files to GitHub

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+    return (
+        <p>
+            TESTEEEE
+        </p>
+    )
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,6 @@
+import {render} from "react-dom";
+import App from "./components/App";
+import './assets/css/reset.css';
+import './assets/css/style.css';
+
+render(<App/>, document.querySelector('.root'));
